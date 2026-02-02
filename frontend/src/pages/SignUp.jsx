3955781eabf4167875884acc7a2922773052ac79
Saifurrahman <saifur13233@gmail.com>
2026-02-02 12:06:38 +0530

    Created routes of signIn, singnUp, logout routes, and setup the background image for there pages

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import bg from "../assets/authBg.png";
+
+function SignUp() {
+  return (
+    <div
+      className="w-full h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}
+    ></div>
+  );
+}
+
+export default SignUp;
